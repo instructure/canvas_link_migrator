68ddb35ad3a6f6559cf4c7be1a081ad349540634
Paul Gray <paul.gray@instructure.com>
2023-07-17 09:23:33 -0400

    Add ci builds

---
+++ bin/publish.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# shellcheck shell=bash
+
+set -e
+
+current_version=$(ruby -e "require '$(pwd)/lib/canvas_link_migrator/version.rb'; puts CanvasLinkMigrator::VERSION;")
+existing_versions=$(gem list --exact canvas-link-migrator --remote --all | grep -o '\((.*)\)$' | tr -d '() ')
+
+if [[ $existing_versions == *$current_version* ]]; then
+  echo "Gem has already been published ... skipping ..."
+else
+  gem build ./canvas_link_migrator.gemspec
+  find canvas-link-migrator-*.gem | xargs gem push
+fi
---
+++ lib/canvas_link_migrator/version.rb
@@ -0,0 +1,3 @@
+module CanvasLinkMigrator
+  VERSION = "0.0.1"
+end
