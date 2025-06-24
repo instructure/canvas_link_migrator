0a808a700cfdd3111c5bfbcba27c211a284ca26a
pzaj-inst <peter.zaj@instructure.com>
2025-06-24 16:12:11 +0200

    Fix media_id extraction in media_map method to use source instead of destination and to check if the file is a hash property

--- lib/canvas_link_migrator/resource_map_service.rb
+++ lib/canvas_link_migrator/resource_map_service.rb
@@ -94,7 +94,7 @@ module CanvasLinkMigrator
 
     def media_map
       @media_map ||= resources["files"].each_with_object({}) do |(_mig_id, file), map|
-        media_id = file.dig("destination", "media_entry_id")
+        media_id = file.dig("source", "media_entry_id") if file.is_a?(Hash)
         next unless media_id
         map[media_id] = file
       end
